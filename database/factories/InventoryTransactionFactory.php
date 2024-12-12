ad4c3deb13883757ce024c0ef3c34f6d17ffca0b
Eduardo <centurionjaime@gmail.com>
2024-12-11 23:20:34 -0500

    Agregar TimeSeeder y registrar tiempos de ejecución en el seeder de transacciones de inventario

--- database/factories/InventoryTransactionFactory.php
+++ database/factories/InventoryTransactionFactory.php
@@ -9,6 +9,8 @@ use App\Models\Product;
 use App\Models\ProductUnitPriceByMeasurement;
 use App\Models\UnitMeasurement;
 use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\InventoryTransaction>
@@ -22,17 +24,33 @@ class InventoryTransactionFactory extends Factory
      */
     public function definition(): array
     {
+
+
+        $logId = DB::table('time_seeders')->insertGetId([
+            'seeder_name' => 'UserSeeder',
+            'started_at' => Carbon::now(),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
         $transactionType = $this->faker->randomElement(['input', 'output']);
         $transactionClase = $transactionType === 'input'? 'purchase': $this->faker->randomElement(['sale', 'production']);
 
-        return [
+        $result = [
             'productUnitPriceId' => ProductUnitPriceByMeasurement::inRandomOrder()->first()->unitMeasurementId,
             'userId' => User::inRandomOrder()->first()->id,
             'transactionType' => $transactionType,
             'transactionClase' => $transactionClase,
             'transactionCount' => $this->faker->randomFloat(2, 1, 100),
-            'transactionDate' => $this->faker->dateTimeBetween('2024-01-01', '2024-11-30')->format('Y-m-d'),
+            // 'transactionDate' => $this->faker->dateTimeBetween('2024-01-01', '2024-11-30')->format('Y-m-d'),
+             'transactionDate' => $this->faker->dateTimeBetween('2010-01-01', '2024-05-30')->format('Y-m-d'),
 
         ];
+         DB::table('time_seeders')->where('id', $logId)->update([
+            'finished_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+
+        return $result;
+
     }
 }
--- database/seeders/InventoryTransactionSeeder.php
+++ database/seeders/InventoryTransactionSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\InventoryTransaction;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class InventoryTransactionSeeder extends Seeder
 {
@@ -14,6 +16,18 @@ class InventoryTransactionSeeder extends Seeder
     public function run(): void
     {
         //
-        InventoryTransaction::factory(500)->create();
+          // Registrar el inicio del seeder
+        //   $logId = DB::table('time_seeders')->insertGetId([
+        //     'seeder_name' => 'UserSeeder',
+        //     'started_at' => Carbon::now(),
+        //     'created_at' => Carbon::now(),
+        //     'updated_at' => Carbon::now(),
+        // ]);
+        InventoryTransaction::factory(55000)->create();
+         // Registrar el fin del seeder
+        //  DB::table('time_seeders')->where('id', $logId)->update([
+        //     'finished_at' => Carbon::now(),
+        //     'updated_at' => Carbon::now(),
+        // ]);
     }
 }
