c771084d870049502894f37133e2f9814a8fa604
Eduardo <centurionjaime@gmail.com>
2024-12-11 23:53:50 -0500

    Aumentar la cantidad de transacciones creadas en el seeder de inventario de 4000 a 6000

--- database/seeders/InventoryTransactionSeeder.php
+++ database/seeders/InventoryTransactionSeeder.php
@@ -23,7 +23,7 @@ class InventoryTransactionSeeder extends Seeder
         //     'created_at' => Carbon::now(),
         //     'updated_at' => Carbon::now(),
         // ]);
-        InventoryTransaction::factory(4000)->create();
+        InventoryTransaction::factory(6000)->create();
          // Registrar el fin del seeder
         //  DB::table('time_seeders')->where('id', $logId)->update([
         //     'finished_at' => Carbon::now(),
